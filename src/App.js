d366aa44423310db3c574f151f2ebe53d6805a62
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-10-11 20:45:30 +0200

    quest finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
+import QuoteCard from './components/QuoteCard';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <QuoteCard />
+      <QuoteCard />
+      <QuoteCard />
+      <QuoteCard />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar(){
+    return (
+        <a href="https://fr.wikipedia.org/wiki/Les_Simpson">
+            <nav className="Navbar">Simpsons Quotes</nav>
+        </a> 
+    )
+};
+
+export default Navbar;
