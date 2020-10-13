3d110e67e650040598f5929ce335d78bd1004a06
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-10-13 17:43:05 +0200

    React 05 quest partially done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './QuoteCard.css';
 import PropTypes from 'prop-types';
 
-function QuoteCard (props){
-    return (
-        <figure className="QuoteCard">
-            <img src={props.image} alt="{props.character}"/>
-            <figcaption>
-                <blockquote>
-                    {props.quote}.
-                </blockquote>
-                <cite>{props.characterFirstName} {props.characterLastName}</cite>
-            </figcaption>
-        </figure>
-    )
-};
-
+class QuoteCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            favorite: false,
+        }
+    }
+    render() {
+        return (
+            <figure className="QuoteCard">
+                <img src={this.props.image} alt="{this.props.character}"/>
+                <figcaption>
+                    <blockquote>
+                        {this.props.quote}
+                    </blockquote>
+                    <p>
+                        <cite>{this.props.characterFirstName} {this.props.characterLastName}</cite>
+                        <span 
+                            onClick={event=>{
+                                console.log(this.state.favorite);
+                                const newFavorite = !this.state.favorite;
+                                this.setState({ favorite: newFavorite });
+                                console.log(this.state.favorite);
+                            }}
+                        >&#9733;</span>
+                    </p>
+                </figcaption>
+            </figure>
+        );
+    }
+}
 QuoteCard.propTypes = {
     characterFirstName: PropTypes.string.isRequired,
     characterLastName: PropTypes.string.isRequired,
   };
 export default QuoteCard;
+
+// function QuoteCard (props){
+//     return (
+//         <figure className="QuoteCard">
+//             <img src={props.image} alt="{props.character}"/>
+//             <figcaption>
+//                 <blockquote>
+//                     {props.quote}.
+//                 </blockquote>
+//                 <cite>{props.characterFirstName} {props.characterLastName}</cite>
+//             </figcaption>
+//         </figure>
+//     )
+// };
+
+// QuoteCard.propTypes = {
+//     characterFirstName: PropTypes.string.isRequired,
+//     characterLastName: PropTypes.string.isRequired,
+//   };
+// export default QuoteCard;
