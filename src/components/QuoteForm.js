bc7a5f2408a4569f7132dd6875b34d34bdf6bb51
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-10-14 14:45:11 +0200

    Input length check added

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './QuoteForm.css'
+
+const MAX_LENGTH = 10;
+
 class QuoteForm extends Component {
     constructor(props){
         super(props)
@@ -13,15 +16,19 @@ class QuoteForm extends Component {
         event.preventDefault();
     }
     changeInput=(event)=>{
-        this.setState({inputValue: event.target.value})
+        if (event.target.value.length<=MAX_LENGTH){
+            this.setState({inputValue: event.target.value});
+        }
+       
     }
 
     render() {
+        const inputMax = this.state.inputValue.length >= MAX_LENGTH;
         return (
             <form className="QuoteForm" onSubmit={this.submitForm}>
                 <h3>Character:</h3>
                 <label htmlFor="Character">
-                    <input type="text" value={this.state.inputValue} onChange={this.changeInput}></input>
+                    <input className={inputMax?'input-max':'input-ok'} type="text" value={this.state.inputValue} onChange={this.changeInput}></input>
                 </label>
             </form>
         );
